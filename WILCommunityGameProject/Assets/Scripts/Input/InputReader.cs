8a7ed5327e1673478dd38384274fab679a807cf7
Fate2664 <18pielou@gmail.com>
2026-04-09 13:18:32 +0200

    Added project

---
+++ WILCommunityGameProject/Assets/Scripts/Input/InputReader.cs
@@ -0,0 +1,114 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+namespace WILCommunityGame
+{
+    public class InputReader : MonoBehaviour, PlayerInputActions.IPlayerActions, PlayerInputActions.IUIActions
+    {
+        #region Class Variables
+
+        [SerializeField] private bool holdToSprint = true;
+        
+        public PlayerInputActions InputActions { get; private set; }
+        public Vector2 MovementInput {get ; private set;}
+        public bool SprintToggledOn { get; private set; }
+        public bool NextPressed { get; private set; }
+        public bool InteractPressed { get; private set; }
+
+        #endregion
+
+        #region Startup & Update Methods
+
+        private void OnEnable()
+        {
+            if (InputActions == null)
+            {
+                InputActions = new PlayerInputActions();
+                InputActions.Player.SetCallbacks(this);
+                InputActions.UI.SetCallbacks(this);
+            }
+            InputActions.Enable();
+        }
+        
+        private void OnDisable()
+        {
+            if (InputActions != null)
+            {
+                InputActions.Disable();
+            }
+        }
+
+        private void LateUpdate()
+        {
+            NextPressed = false;
+            InteractPressed = false;
+        }
+        #endregion
+
+        #region Gameplay Inputs
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            MovementInput = context.ReadValue<Vector2>();
+        }
+
+        public void OnSprint(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                SprintToggledOn = holdToSprint || !SprintToggledOn;
+            }
+            else if (context.canceled)
+            {
+                SprintToggledOn = !holdToSprint && SprintToggledOn;
+            }
+        }
+
+        public void OnInteract(InputAction.CallbackContext context)
+        {
+            if (!context.performed) return;
+            InteractPressed = true;
+        }
+
+        #endregion
+
+        #region UI Inputs
+
+        public void OnExit(InputAction.CallbackContext context)
+        {
+            return;
+        }
+
+        public void OnClick(InputAction.CallbackContext context)
+        {
+            return;
+        }
+
+        public void OnRightClick(InputAction.CallbackContext context)
+        {
+            return;
+        }
+
+        public void OnNext(InputAction.CallbackContext context)
+        {
+            if (!context.performed) return;
+            
+            NextPressed = true;
+        }
+
+        public void OnNavigate(InputAction.CallbackContext context)
+        {
+            return;
+        }
+
+        public void OnApply(InputAction.CallbackContext context)
+        {
+        }
+
+        #endregion
+
+    }
+}
+
